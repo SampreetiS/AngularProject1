c27f25e3b366dba7ca871b025a55412b2fef5804
SampreetiS <sampreetis.savukar@dxc.com>
2025-03-04 12:30:41 +0530

    Save changes before Angular update

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,7 +7,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [RouterLink, NavbarComponent, LoginComponent, SignUpComponent],
+  imports: [RouterLink, NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
